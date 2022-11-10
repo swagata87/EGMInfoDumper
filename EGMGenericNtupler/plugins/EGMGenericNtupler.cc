4042bb4605e2dbbe67cd1e6941d0589fa0f31639
swagata87 <physics.swagata@gmail.com>
2022-11-10 10:57:12 +0100

    quick example for PU correction

--- EGMGenericNtupler/plugins/EGMGenericNtupler.cc
+++ EGMGenericNtupler/plugins/EGMGenericNtupler.cc
@@ -6,15 +6,8 @@
 #include <memory>
 
 // user include files
-//#include "DataFormats/TrackReco/interface/Track.h"
-//#include "DataFormats/GsfTrackReco/interface/GsfTrackFwd.h"
-//#include "DataFormats/DetId/interface/DetId.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/one/EDAnalyzer.h"
-//#include "RecoEcal/EgammaCoreTools/interface/EcalClusterTools.h"
-//#include "RecoEcal/EgammaCoreTools/interface/EcalTools.h"
-//#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
-//#include "RecoEcal/EgammaCoreTools/interface/EcalClusterLazyTools.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
@@ -22,24 +15,9 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
-//#include "RecoEgamma/EgammaIsolationAlgos/interface/EgammaRecHitIsolation.h"
-//#include "DataFormats/EcalRecHit/interface/EcalRecHit.h"
-//#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
 #include "FWCore/Framework/interface/ESHandle.h"
-#include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
-#include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
-#include "SimDataFormats/GeneratorProducts/interface/LHEEventProduct.h"
-#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
-#include "DataFormats/Math/interface/deltaR.h"
-//#include "CondFormats/EcalObjects/interface/EcalPFRecHitThresholds.h"
-//#include "CondFormats/DataRecord/interface/EcalPFRecHitThresholdsRcd.h"
 #include "DataFormats/PatCandidates/interface/Photon.h"
-//#include "DataFormats/METReco/interface/BeamHaloSummary.h"
 #include "TTree.h"
-//#include "DataFormats/PatCandidates/interface/Electron.h"
-#include <cmath>
-#include "TVector3.h"
-#include "TLorentzVector.h"
 #include "CommonTools/Egamma/interface/EffectiveAreas.h"
 
 class EGMGenericNtupler : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
@@ -50,44 +28,16 @@ public:
   
   edm::Service<TFileService> fs;
   TTree   *tree = fs->make<TTree>("EventTree", "EventData");
-
-  //
-  //  std::vector<float>  puTrue;
   //std::vector<float>  rho;
-  //  std::vector<int>    gen_status;
-  // std::vector<int>    gen_pdgid;
-  //std::vector<float>  gen_pt;
-
-  /// photon
-  //  std::vector<float>  ptRecoPho_by_ptGenPho;
-  //std::vector<float>  dR_recoPho_genPho;
-  //std::vector<int>  pho_genmatched;
-  //std::vector<std::vector<int> > pho_IDbits;
-  //std::vector<int> pho_IDLoose;
-  //std::vector<int> pho_IDMedium;
-  //std::vector<int> pho_IDTight;
   //std::vector<double> pho_Pt;
-  //  std::vector<double> pho_HoE;
-  //std::vector<double> pho_SCrawE;
-  //std::vector<double> pho_ChargedHadronWorstVtxIso;
-  //std::vector<double> pho_ChargedHadronIso;
-  //std::vector<double> pho_PhotonIso;
-  //std::vector<double> pho_EtaWidth;
-  //std::vector<double> pho_PhiWidth;
-  //std::vector<double> pho_Phi;
-  //std::vector<double> pho_ScEn;
-  //std::vector<double> pho_ScEta;
-  //std::vector<double> pho_SigmaIetaIeta;
 
 private:
   virtual void beginJob() override;
   virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
   virtual void endJob() override;
+  double getEAcorrectedVar(EffectiveAreas thisEA, double thisAbsEta, double thisRho, double thisVar);
 
-  //  virtual  void setbit(UShort_t& x, UShort_t bit);
-  
   // ----------member data ---------------------------
-
   EffectiveAreas _effectiveAreas_charged;
   EffectiveAreas _effectiveAreas_neutral;
   EffectiveAreas _effectiveAreas_photon;
@@ -101,184 +51,46 @@ EGMGenericNtupler::EGMGenericNtupler(const edm::ParameterSet& iConfig)
   _effectiveAreas_neutral((iConfig.getParameter<edm::FileInPath>("effAreas_neutral")).fullPath()),
   _effectiveAreas_photon((iConfig.getParameter<edm::FileInPath>("effAreas_photon")).fullPath()),
   rhoLabel_(consumes<double>(iConfig.getParameter<edm::InputTag>("rhoLabel"))),
-  //eleToken_(consumes<edm::View<pat::Electron> >(iConfig.getParameter<edm::InputTag>("electrons"))),
   phoToken_(consumes<edm::View<pat::Photon> >(iConfig.getParameter<edm::InputTag>("Photons")))
-  //ootphoToken_(consumes<edm::View<pat::Photon> >(iConfig.getParameter<edm::InputTag>("ootPhotons"))),
-  //puCollection_(consumes<std::vector<PileupSummaryInfo> >(iConfig.getParameter<edm::InputTag>("pileupCollection"))),
-  //genParticlesCollection_(consumes<std::vector<reco::GenParticle> >(iConfig.getParameter<edm::InputTag>("genParticleSrc")))
-
 {
-
-
-
-  //  tree->Branch("puTrue_", &puTrue);
   //tree->Branch("rho_", &rho);
-  //tree->Branch("gen_status_",&gen_status);
-  //tree->Branch("gen_pdgid_",&gen_pdgid);
-  // tree->Branch("gen_pt_",&gen_pt);
-
-  ////photon
-  /*
-  tree->Branch("ptRecoPho_by_ptGenPho_",&ptRecoPho_by_ptGenPho);
-  tree->Branch("dR_recoPho_genPho_",&dR_recoPho_genPho);
-  tree->Branch("pho_genmatched_",&pho_genmatched);
-  tree->Branch("pho_IDbits_", &pho_IDbits);
-  tree->Branch("pho_IDLoose_", &pho_IDLoose);
-  tree->Branch("pho_IDMedium_", &pho_IDMedium);
-  tree->Branch("pho_IDTight_", &pho_IDTight);
-  tree->Branch("pho_Pt_",&pho_Pt);
-  tree->Branch("pho_R9_",&pho_R9);
-  tree->Branch("pho_HoE_",&pho_HoE);
-  tree->Branch("pho_SCrawE_",&pho_SCrawE);
-  tree->Branch("pho_ChargedHadronWorstVtxIso_",&pho_ChargedHadronWorstVtxIso);
-  tree->Branch("pho_ChargedHadronIso_",&pho_ChargedHadronIso);
-  tree->Branch("pho_PhotonIso_",&pho_PhotonIso);
-  tree->Branch("pho_EtaWidth_",&pho_EtaWidth);
-  tree->Branch("pho_PhiWidth_",&pho_PhiWidth);
-  tree->Branch("pho_Phi_",&pho_Phi);
-  tree->Branch("pho_ScEn_",&pho_ScEn);
-  tree->Branch("pho_ScEta_",&pho_ScEta);
-  tree->Branch("pho_SigmaIetaIeta_",&pho_SigmaIetaIeta);
-  */
+  //  tree->Branch("pho_Pt_",&pho_Pt);
 }
 
 EGMGenericNtupler::~EGMGenericNtupler()
 {
 }
 
-//
-// member functions
-//
-
 // ------------ method called for each event  ------------
 void
 EGMGenericNtupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-
   using namespace edm;
-  
-  //
-  //  puTrue.clear();
-  // rho.clear();
-  // gen_status.clear();
-  // gen_pdgid.clear();
-  // gen_pt.clear();
+    // rho.clear();
+  //  pho_Pt.clear();
 
-  //
-  // photon
-  //
-  /*
-  pho_genmatched.clear();
-  dR_recoPho_genPho.clear();
-  ptRecoPho_by_ptGenPho.clear();
-  pho_IDbits.clear();
-  pho_IDLoose.clear();
-  pho_IDMedium.clear();
-  pho_IDTight.clear();
-  pho_Pt.clear();
-  pho_R9.clear();
-  pho_HoE.clear();
-  pho_SCrawE.clear();
-  pho_ChargedHadronWorstVtxIso.clear();
-  pho_ChargedHadronIso.clear();
-  pho_PhotonIso.clear();
-  pho_EtaWidth.clear();
-  pho_PhiWidth.clear();
-  pho_Phi.clear();
-  pho_ScEn.clear();
-  pho_ScEta.clear();
-  pho_SigmaIetaIeta.clear();
-  */
-  // edm::Handle<std::vector<reco::GenParticle> > genParticlesHandle;
-  
-  ////
-  //  if ( ! iEvent.isRealData()) {
-  //edm::Handle<std::vector<PileupSummaryInfo> > genPileupHandle;
-  //iEvent.getByToken(puCollection_, genPileupHandle);
-  //if (genPileupHandle.isValid()) {
-  //  for (std::vector<PileupSummaryInfo>::const_iterator pu = genPileupHandle->begin(); pu != genPileupHandle->end(); ++pu) {
-  //	puTrue.push_back(pu->getTrueNumInteractions());
-  //  }
-  //}
-    
-  //iEvent.getByToken(genParticlesCollection_, genParticlesHandle);
-    
-  //   if (genParticlesHandle.isValid()) {
-  //  for (std::vector<reco::GenParticle>::const_iterator ip = genParticlesHandle->begin(); ip != genParticlesHandle->end(); ++ip) {
-  //	const reco::Candidate *p = (const reco::Candidate*)&(*ip);
-  //	gen_status.push_back(p->status());
-  //	gen_pdgid.push_back(p->pdgId());
-  //	gen_pt.push_back(p->pt());
-  //  }  
-  //}
-  //} // check real data
-  
   edm::Handle<double> rhoHandle;
   iEvent.getByToken(rhoLabel_, rhoHandle);
   //  rho.push_back(*(rhoHandle.product()));
-  std::cout << "rho = " << *(rhoHandle.product()) << std::endl;
+  double rho = rhoHandle.isValid() ? *rhoHandle : 0;
+  std::cout << "rho " << rho << std::endl;
+
   ////photon
   for(const auto& pho : iEvent.get(phoToken_) ) {
 
-    // bool pass_tight = pho.photonID("cutBasedPhotonID-Fall17-94X-V2-tight");
-    //pho_IDbits.push_back({pho.userInt("cutBasedPhotonID-Fall17-94X-V2-loose"),
-    //	  pho.userInt("cutBasedPhotonID-Fall17-94X-V2-medium"),
-    //	  pho.userInt("cutBasedPhotonID-Fall17-94X-V2-tight")});
-    
-    //    int thispho_genmatched=0;
-    // double thispho_min_dr=9999.9;
-    // double thispho_ptR=9999.9;
-    //if ( ! iEvent.isRealData()) {
-      
-    //if (genParticlesHandle.isValid()) {
-    //	for (std::vector<reco::GenParticle>::const_iterator ip = genParticlesHandle->begin(); ip != genParticlesHandle->end(); ++ip) {
-    //	  const reco::Candidate *p = (const reco::Candidate*)&(*ip);
-	  
-    //	  if ( (p->status()==1) && ((abs(p->pdgId())) == 22) )  {
-	    
-    //	    double this_dr=reco::deltaR(pho,*p);
-    //	    if (this_dr<thispho_min_dr) {
-    //	      thispho_min_dr=this_dr;
-    //	      thispho_ptR=pho.pt()/p->pt();
-    //	    }
-    //	  }
-    //	}
-    //}
-    //}
-    
-    //    if ( (thispho_min_dr<0.1) && (thispho_ptR>0.7) && (thispho_ptR<1.3) )  thispho_genmatched=1;
-    
-    // pho_genmatched.push_back(thispho_genmatched);
-    //dR_recoPho_genPho.push_back(thispho_min_dr);
-    //ptRecoPho_by_ptGenPho.push_back(thispho_ptR);
-    //pho_Pt.push_back(pho.pt());
-    //pho_Phi.push_back(pho.phi());
-    //pho_ScEn.push_back(pho.superCluster()->energy());
-    //pho_ScEta.push_back(pho.superCluster()->eta());
-    //pho_SigmaIetaIeta.push_back(pho.full5x5_sigmaIetaIeta());
-    //pho_HoE.push_back(pho.hadTowOverEm());
-    //pho_R9.push_back(pho.full5x5_r9());
-    //pho_EtaWidth.push_back(pho.superCluster()->etaWidth());
-    //pho_PhiWidth.push_back(pho.superCluster()->phiWidth());
-    //pho_PhotonIso.push_back(pho.photonIso());  
-    //pho_ChargedHadronIso.push_back(pho.chargedHadronIso());  
+    double absEta = std::abs(pho.superCluster()->eta());
+    std::cout << "photon pT/ absEta " << pho.pt() << " " << absEta << std::endl;
 
-    //    if  ( (pho.chargedHadronIso() < 0 ) || isnan(pho.chargedHadronIso()) )
-    //{
-	std::cout << "\n\n ** pho.chargedHadronIso() = " << pho.chargedHadronIso() << " pho.chargedHadronPFPVIso() = " << pho.chargedHadronPFPVIso() << std::endl;
-	std::cout << "pho.pt() " << pho.pt() << " pho.photonIso() " << pho.photonIso() << " pho.superCluster()->eta() " << pho.superCluster()->eta() << std::endl;
-	std::cout << "tightID " << pho.photonID("cutBasedPhotonID-Fall17-94X-V2-tight") << std::endl;
-	std::cout << "looseID " << pho.photonID("cutBasedPhotonID-Fall17-94X-V2-loose") << std::endl;
-	std::cout << "\n"; 
-	//}
-    //    pho_ChargedHadronWorstVtxIso.push_back(pho.chargedHadronWorstVtxIso());
-    //  pho_SCrawE.push_back(pho.superCluster()->rawEnergy());    
-   
+    double chargedHadronIsoWithEA = getEAcorrectedVar(_effectiveAreas_charged, absEta, rho, pho.chargedHadronIso());
+    double neutralHadronIsoWithEA = getEAcorrectedVar(_effectiveAreas_neutral, absEta, rho, pho.neutralHadronIso());
+    double photonIsoWithEA        = getEAcorrectedVar(_effectiveAreas_photon,  absEta, rho, pho.photonIso());
 
-          
+    std::cout << "charged/neutral/photon isolation (PU corrected) " << 
+      chargedHadronIsoWithEA << " " << neutralHadronIsoWithEA << " " << photonIsoWithEA << std::endl;
+    std::cout << "H/E and SigmaIEtaIEta " << pho.hcalOverEcalBc() << " " << pho.full5x5_sigmaIetaIeta() << std::endl;
+    std::cout << "\n"; 
   }
 
-  
   tree->Fill();
   
 #ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
@@ -287,19 +99,24 @@ EGMGenericNtupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
 #endif
 }
 
+//function for PU correction
+double EGMGenericNtupler::getEAcorrectedVar(EffectiveAreas thisEA, double thisAbsEta, double thisRho, double thisVar) {
+  double valEA = thisEA.getEffectiveArea(thisAbsEta);
+  double thisVar_EAcorr = std::max(0.0, thisVar - thisRho*valEA);
+  return thisVar_EAcorr;
+}
+
 
 // ------------ method called once each job just before starting event loop  ------------
 void
 EGMGenericNtupler::beginJob()
 {
 }
-
 // ------------ method called once each job just after ending the event loop  ------------
 void
 EGMGenericNtupler::endJob()
 {
 }
-
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
 EGMGenericNtupler::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
@@ -308,8 +125,6 @@ EGMGenericNtupler::fillDescriptions(edm::ConfigurationDescriptions& descriptions
   edm::ParameterSetDescription desc;
   desc.setUnknown();
   descriptions.addDefault(desc);
-
-
 }
 
 //define this as a plug-in
--- EGMGenericNtupler/python/ConfFile_cfg.py
+++ EGMGenericNtupler/python/ConfFile_cfg.py
@@ -4,49 +4,23 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.load("Geometry.CaloEventSetup.CaloTopology_cfi");
-process.load("Geometry.CaloEventSetup.CaloGeometry_cfi");
-process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi");
-process.load("Configuration.Geometry.GeometryECALHCAL_cff")
-
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.load('PhysicsTools.HepMCCandAlgos.genParticles_cfi')
-
-process.MessageLogger.cerr.FwkReport.reportEvery = 5000
 
 from Configuration.AlCa.GlobalTag import GlobalTag
-
 process.GlobalTag = GlobalTag(process.GlobalTag, '122X_mcRun3_2021_realistic_v9', '')     # 2022 MC
-
-
-
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-
-
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-
-#                                '/store/mc/Run3Winter22MiniAOD/GJet_Pt-10to40_DoubleEMEnriched_TuneCP5_13p6TeV_pythia8/MINIAODSIM/FlatPU0to70_122X_mcRun3_2021_realistic_v9-v2/2430000/ad93b873-e478-4f1d-82ea-2962217328a2.root'
                                 '/store/mc/Run3Winter22MiniAOD/GJet_Pt-10to40_DoubleEMEnriched_TuneCP5_13p6TeV_pythia8/MINIAODSIM/FlatPU0to70_122X_mcRun3_2021_realistic_v9-v2/2430000/b30da4dc-1771-491e-b4ce-f424c076aab5.root',
-#                                '/store/mc/Run3Winter22MiniAOD/GJet_Pt-10to40_DoubleEMEnriched_TuneCP5_13p6TeV_pythia8/MINIAODSIM/FlatPU0to70_122X_mcRun3_2021_realistic_v9-v2/2430000/b52ef7ee-f58e-4a94-95fa-dd980c814966.root',
-
                                 )
 )
 
-#process.source.eventsToProcess = cms.untracked.VEventRange('1:500-1:700')
-
 process.demo = cms.EDAnalyzer('EGMGenericNtupler',
        rhoLabel  = cms.InputTag("fixedGridRhoFastjetAll"),
-       #electrons = cms.InputTag('slimmedElectrons'),
-       #pileupCollection     = cms.InputTag("slimmedAddPileupInfo"),
-       #genParticleSrc       = cms.InputTag("prunedGenParticles"),
        Photons       = cms.InputTag("slimmedPhotons"),
-       effAreas_charged = cms.FileInPath("/afs/cern.ch/user/s/swmukher/work/spoonFeedArrogantUsers/CMSSW_12_4_6/src/EGMInfoDumper/EGMGenericNtupler/Fall17/effAreaPhotons_cone03_pfChargedHadrons_90percentBased_V2.txt"),
-       effAreas_neutral = cms.FileInPath("/afs/cern.ch/user/s/swmukher/work/spoonFeedArrogantUsers/CMSSW_12_4_6/src/EGMInfoDumper/EGMGenericNtupler/Fall17/effAreaPhotons_cone03_pfNeutralHadrons_90percentBased_V2.txt"),
-       effAreas_photon = cms.FileInPath("/afs/cern.ch/user/s/swmukher/work/spoonFeedArrogantUsers/CMSSW_12_4_6/src/EGMInfoDumper/EGMGenericNtupler/Fall17/effAreaPhotons_cone03_pfPhotons_90percentBased_V2.txt"),
-       #ootPhotons       = cms.InputTag("slimmedOOTPhotons"),
+       effAreas_charged = cms.FileInPath("RecoEgamma/PhotonIdentification/data/Fall17/effAreaPhotons_cone03_pfChargedHadrons_90percentBased_V2.txt"),
+       effAreas_neutral = cms.FileInPath("RecoEgamma/PhotonIdentification/data/Fall17/effAreaPhotons_cone03_pfNeutralHadrons_90percentBased_V2.txt"),
+       effAreas_photon = cms.FileInPath("RecoEgamma/PhotonIdentification/data/Fall17/effAreaPhotons_cone03_pfPhotons_90percentBased_V2.txt"),
                               )
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string('genericEGMntuple_mc.root'))
-
 process.p = cms.Path(process.demo)
